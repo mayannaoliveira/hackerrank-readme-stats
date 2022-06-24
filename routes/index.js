ddda786db04c501669d832ef222c56202228182d
Yash Eksambekar <yasheksambekar@gmail.com>
2022-06-24 22:05:16 +0530

    modified functions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
-const express = require("express");
 const badges = require("../src/generators/hackerrank");
-const router = express.Router();
 
-router.get("/hackerrank/badges", (req, res) => {
+module.exports = (req, res) => {
   res.contentType("image/svg+xml");
   badges(req.query.username, (result) => res.send(result));
-});
-
-module.exports = router;
+};
